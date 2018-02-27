3d780bcc46347e4a7d2cb4cf31b33b6c9ec3ec2d
Jeffrey.Dube <jeffdube_20@hotmail.com>
2018-02-27 09:02:20 -0500

    Ajout de ma feature qui est un quizz.

---
+++ Site web/quizz.php
@@ -0,0 +1,47 @@
+<form action="quizz_result.php" method="post" id="quiz">
+
+<li>
+
+
+
+<h3>HTML Stands for...</h3>
+
+<div>
+
+<input type="radio" name="question-1-answers" id="question-1-answers-A" value="A" />
+
+<label for="question-1-answers-A">A) Hypertext Markup Language </label>
+
+</div>
+
+<div>
+
+<input type="radio" name="question-1-answers" id="question-1-answers-B" value="B" />
+
+<label for="question-1-answers-B">B) Hypertext Markup</label>
+
+</div>
+
+<div>
+
+<input type="radio" name="question-1-answers" id="question-1-answers-C" value="C" />
+
+<label for="question-1-answers-C">C) Hypertext Programming</label>
+
+</div>
+
+<div>
+
+<input type="radio" name="question-1-answers" id="question-1-answers-D" value="D" />
+
+<label for="question-1-answers-D">D) None of the above</label>
+
+</div>
+
+
+
+</li>
+
+<input type="submit" value="Submit Quiz" />
+
+</form>
